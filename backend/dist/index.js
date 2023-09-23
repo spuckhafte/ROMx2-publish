50d0e9d38d0db5563b80e7f7f3a091a9d3986d89
spuckhafte <rakshit240405@gmail.com>
2023-09-24 01:11:52 +0530

    ::port::

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,4 @@
+var _a;
 import EVaultChain from "./helpers/EVaultChain.js";
 import { Plugboard } from "plugboard.io";
 export const EVault = new EVaultChain();
@@ -5,4 +6,4 @@ const plugboard = new Plugboard('dist/sockets', {
     cors: { origin: "*" },
     maxHttpBufferSize: 1e7,
 });
-plugboard.start(3000, () => console.log('[server started]'));
+plugboard.start(+((_a = process.env) === null || _a === void 0 ? void 0 : _a.PORT) || 3000, () => console.log('[server started]'));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,9 @@ import { Plugboard } from "plugboard.io";
 
 export const EVault = new EVaultChain();
 
-const plugboard = new Plugboard('dist/sockets', { 
+const plugboard = new Plugboard('dist/sockets', {
     cors: { origin: "*" },
     maxHttpBufferSize: 1e7,
 });
 
-plugboard.start(3000, () => console.log('[server started]'));
+plugboard.start(+(process.env?.PORT as unknown as number) || 3000, () => console.log('[server started]'));
